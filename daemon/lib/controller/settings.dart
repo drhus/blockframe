2464ba9a09bffebbc38e3a66842df69b3afeed97
Daniel Vieira <daniel@vieira.pw>
2018-03-23 07:38:56 -0300

    Create log directory

--- daemon/lib/controller/settings.dart
+++ daemon/lib/controller/settings.dart
@@ -9,20 +9,28 @@ class Settings {
   static Settings get instance => _instance;
 
   final Logger logger = new Logger('blockframe');
-  final File log = new File('log.txt');
+  final File log = new File('log/log.txt');
 
   Settings._private() {
 
-    log.openWrite();
+    if (! log.existsSync()) {
+
+      log.createSync(recursive: true);
+
+    }
+
+    IOSink sink = log.openWrite(mode: FileMode.APPEND);
 
     Logger.root.level = Level.INFO;
 
-    Logger.root.onRecord.listen((LogRecord rec) {
+    Logger.root.onRecord.listen((LogRecord rec) async {
 
       String contents = '${rec.level.name}: ${rec.time}: ${rec.message}';
 
       print(contents);
-      log.writeAsStringSync(contents);
+      sink.writeln(contents);
+
+      await sink.flush();
 
     });
 
